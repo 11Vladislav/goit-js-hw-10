0cc14fe03649e7ea01179ff4189b699158976443
Vladislav Ponomarev <vlad11ponomarev@gmail.com>
2022-05-13 17:54:53 +0300

    update hometask

--- src/index.js
+++ src/index.js
@@ -2,7 +2,70 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './js/fetch-countries';
-import countriesListTpl from './templates/countries-list.hbs';
-import countryInfoTpl from './templates/country-info.hbs';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 500;
+const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countriesList: document.querySelector('.js-country-list'),
+  countryInfo: document.querySelector('.js-country-info'),
+};
+
+refs.searchBox.addEventListener('input', debounce(handleSearch, DEBOUNCE_DELAY));
+
+function clearData() {
+  refs.countriesList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
+}
+
+function handleSearch(event) {
+  const inputValue = event.target.value.trim();
+  if (inputValue === '') {
+    clearData();
+    return;
+  }
+  fetchCountries(inputValue)
+    .then(countries => {
+      if (countries.length > 10) {
+        clearData();
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific query!');
+        return;
+      }
+      else if (countries.length === 1) {
+        clearData();
+        renderCountry(countries[0]);
+      }
+      else {
+        clearData();
+        renderCountries(countries);
+      }
+    })
+    .catch(error => {
+      clearData();
+      Notiflix.Notify.failure('Oops, there is no country with that name!');
+    }
+    );
+}
+
+  function renderCountry(country) {
+    refs.countryInfo.innerHTML = `
+      <div class="info-title">
+        <img src = "${country.flags.svg}" alt = Flag of"${country.name.official} class = "flag" ">
+        <h1>${country.name.official}</h1>
+        <p><span>Capital:</span> ${country.capital}</p>
+        <p><span>Population:</span> ${country.population}</p>
+        <p><span>Languages:</span> ${country.languages}</p>
+        </div>
+    `;
+    console.log(country.languages);
+  }
+
+function renderCountries(countries) {
+  countries.forEach(country => {
+    refs.countriesList.innerHTML = `
+      <li>
+         <img src = "${country.flags.svg}" alt = Flag of"${country.name.official}">
+        <span>${country.name.official}</span>
+        </li>
+    `;
+  });
+}
